a9cea7967c1e5f1745e422b5b7181e4b3e57833b
RubenDarioCoder <dariomacielr@gmail.com>
2026-08-02 17:39:16 -0300

    Change default database name to 'tienda_ropa'

--- src/config/database.js
+++ src/config/database.js
@@ -15,7 +15,7 @@ if (process.env.MYSQL_URL) {
         host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_NAME || 'LYM',
+        database: process.env.DB_NAME || 'tienda_ropa',
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0,
